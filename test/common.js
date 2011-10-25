b205b01b1721ecdeaee4000a5430701e8dad20ea
PlNG <tempeml@hotmail.com>
2011-10-25 12:13:45 -0300

    Formatting for consistency.

--- test/common.js
+++ test/common.js
@@ -1,8 +1,9 @@
-var path = require('path')
-  , sys = require('sys');
-
-require.paths.unshift(path.dirname(__dirname)+'/lib');
-
+var path = require("path"),
+  sys = require("sys");
+require.paths.unshift(path.dirname(__dirname) + "/lib");
 global.puts = sys.puts;
-global.p = function() {sys.error(sys.inspect.apply(null, arguments));};
-global.assert = require('assert');
+global.p = function () {
+	"use strict";
+	sys.error(sys.inspect.apply(null, arguments));
+};
+global.assert = require("assert");
